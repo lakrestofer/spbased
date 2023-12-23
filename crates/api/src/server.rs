bc4bae4047067ced6b2b1cac1ab2eff0de7481fe
lakrestofer <lakrestofer@gmail.com>
2023-12-23 20:58:00 +0100

    implement inital configuration layer for server

--- crates/api/src/server.rs
+++ crates/api/src/server.rs
@@ -1,12 +1,56 @@
+use std::path::PathBuf;
+
 use crate::collection::{CollectionServer, CollectionService};
 use migration::{Migrator, MigratorTrait};
 use sea_orm::Database;
+use serde::{Deserialize, Serialize};
 use tonic::transport::Server;
 
-pub async fn run_server() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:10000".parse().unwrap();
+#[derive(Serialize, Deserialize)]
+pub struct ServerConfig {
+    server_address: String,
+    // #[serde(skip_serializing)]
+    db_dir: Option<PathBuf>, // path to the directory containing the sqlite db file
+    // #[serde(skip_serializing)]
+    db_name: Option<String>,
+}
 
-    let protocol_str = format!("sqlite://db.sqlite?mode=rwc");
+impl ServerConfig {
+    fn merge(self, other: Self) -> Self {
+        Self {
+            server_address: self.server_address,
+            db_dir: self.db_dir.or(other.db_dir),
+            db_name: self.db_name.or(other.db_name),
+        }
+    }
+}
+
+impl Default for ServerConfig {
+    fn default() -> Self {
+        Self {
+            // TODO mature and figure a better default port
+            server_address: "[::1]:42069".into(),
+            db_dir: Some(dirs::data_local_dir().unwrap().join("spbased")),
+            db_name: Some("db.sqlite".into()),
+        }
+    }
+}
+
+pub async fn run_server(config: ServerConfig) -> Result<(), Box<dyn std::error::Error>> {
+    // merge config with defaults
+    let config = config.merge(ServerConfig::default());
+    // all fields are now Some
+    let server_address = config.server_address;
+    let db_dir = config.db_dir.unwrap();
+    let db_name = config.db_name.unwrap();
+    let db_path = db_dir.join(db_name);
+    let addr = server_address.parse().unwrap();
+
+    // create directories if they do not exist
+    std::fs::create_dir_all(db_dir)?;
+
+    // connect to db and perform migration
+    let protocol_str = format!("sqlite://{}?mode=rwc", db_path.to_str().unwrap());
     let db = Database::connect(protocol_str)
         .await
         .expect("could not connect to db");
@@ -15,8 +59,8 @@ pub async fn run_server() -> Result<(), Box<dyn std::error::Error>> {
         .await
         .expect("Could not perform a fresh migration");
 
+    // build services and launch server
     let collection = CollectionService::new(db);
-
     let collection_server = CollectionServer::new(collection);
 
     Server::builder()
--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -1,6 +1,59 @@
+use std::path::PathBuf;
+
 use api::server;
+use clap::Parser;
+
+use dirs::config_dir;
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser)]
+#[command(name = "Spbased")]
+#[command(author = "la. Krestofer <lagrestofer@gmail.com>")]
+// #[command(version = "1.0")]
+#[command(about = "gRPC daemon for spbased ecosystem. See https://github.com/lakrestofer/spbased for more info", long_about = None)]
+struct Cli {
+    /// Override default config path
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+}
+
+#[derive(Default, Serialize, Deserialize)]
+struct Config {
+    server_config: server::ServerConfig,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    server::run_server().await
+    let cli = Cli::parse();
+
+    let config_dir = config_dir().expect("could not retrieve user config directory. Is platform anything other than linux, macOS or windows?");
+    // override the config location
+    let config_dir = config_dir.join("spbased");
+    let mut config_file = config_dir.join("config.toml");
+
+    if let Some(path) = cli.config {
+        if path.is_file() {
+            config_file = path;
+        }
+    }
+
+    // try to read the config file
+    let config: Config = {
+        match std::fs::read_to_string(&config_file) {
+            // if it already exists, try to parse it
+            Ok(str) => toml::from_str(&str)?,
+            // if doesn't, write the default config to the file
+            _ => {
+                let config = Config::default();
+                // create the dirs
+                std::fs::create_dir_all(&config_dir)?;
+                // write the default config to a file
+                std::fs::write(&config_file, toml::ser::to_string(&config)?)?;
+                config
+            }
+        }
+    };
+
+    // launch the server with the provided config
+    server::run_server(config.server_config).await
 }
