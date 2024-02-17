18729de4521100ef4b53d061710a8bf972a59d1c
lakrestofer <lakrestofer@gmail.com>
2024-02-17 21:48:59 +0100

    reinit flashcards project

--- crates/flashcards-tui/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // this injects cargo version, rustc version etc such that it is reachable for env!
-    vergen::EmitBuilder::builder()
-        .all_build()
-        .all_git()
-        .emit()?;
-    Ok(())
-}
