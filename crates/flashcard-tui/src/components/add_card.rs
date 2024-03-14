3a2503e57418340b614f3ec8c12c1f030e9d5163
lakrestofer <lakrestofer@gmail.com>
2024-03-14 12:57:18 +0100

    add textarea widget and implement initial edit screen

--- crates/flashcard-tui/src/components/add_card.rs
+++ crates/flashcard-tui/src/components/add_card.rs
@@ -1,20 +1,75 @@
-use ratatui::widgets::Paragraph;
+use ratatui::{
+    layout::{Constraint, Flex, Layout},
+    text::Line,
+    widgets::{Block, BorderType, Borders, Padding, Paragraph},
+};
+use tui_textarea::TextArea;
 
 use super::Component;
 use crate::preamble::*;
 
-#[derive(Debug, Default)]
-pub struct AddCard;
+#[derive(Debug)]
+pub struct AddCard<'a> {
+    answer_area: TextArea<'a>,
+    question_area: TextArea<'a>,
+}
 
-impl Boxed for AddCard {}
-impl Component for AddCard {
+impl<'a> Default for AddCard<'a> {
+    fn default() -> Self {
+        let answer_area = TextArea::default();
+        let question_area = TextArea::default();
+        Self {
+            answer_area,
+            question_area,
+        }
+    }
+}
+
+impl<'a> Boxed for AddCard<'a> {}
+impl<'a> Component for AddCard<'a> {
     fn render(
         &self,
         state: &crate::state::State,
         frame: &mut ratatui::prelude::Frame,
         rect: ratatui::prelude::Rect,
     ) {
-        frame.render_widget(Paragraph::new(format!("{:?}", self)), rect);
+        let [left, right] =
+            Layout::horizontal([Constraint::Percentage(75), Constraint::Percentage(25)])
+                .areas(rect);
+
+        let divider = Block::default()
+            .padding(Padding::horizontal(4))
+            .borders(Borders::RIGHT)
+            .border_type(BorderType::Plain);
+
+        frame.render_widget(&divider, left);
+
+        let left = divider.inner(left);
+
+        let [upper_left, lower_left] = Layout::vertical([Constraint::Percentage(40); 2])
+            .flex(Flex::SpaceAround)
+            .areas(left);
+
+        // help text
+        frame.render_widget(Paragraph::new("Border"), right);
+
+        // question block
+        let upper_left_block = Block::default()
+            .title_top(Line::from("Question").centered())
+            .borders(Borders::ALL)
+            .border_type(BorderType::Plain);
+        frame.render_widget(&upper_left_block, upper_left);
+        let upper_left = upper_left_block.inner(upper_left);
+        frame.render_widget(self.question_area.widget(), upper_left);
+
+        // answer block
+        let lower_left_block = Block::default()
+            .title_top(Line::from("Answer").centered())
+            .borders(Borders::ALL)
+            .border_type(BorderType::Plain);
+        frame.render_widget(&lower_left_block, lower_left);
+        let lower_left = lower_left_block.inner(lower_left);
+        frame.render_widget(self.answer_area.widget(), lower_left);
     }
 
     fn handle_key_events(
