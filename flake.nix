9e6971f793d12f442d2be3b51c40db0cad02d735
lakrestofer <lakrestofer@gmail.com>
2024-12-30 00:57:05 +0100

    use nightly rust compiler

--- flake.nix
+++ flake.nix
@@ -16,42 +16,57 @@
   };
 
   # Flake outputs that other flakes can use
-  outputs = { self, flake-schemas, nixpkgs, rust-overlay }:
+  outputs =
+    {
+      self,
+      flake-schemas,
+      nixpkgs,
+      rust-overlay,
+    }:
     let
       # Nixpkgs overlays
       overlays = [
         rust-overlay.overlays.default
         (final: prev: {
-          rustToolchain = final.rust-bin.stable.latest.default.override { extensions = [ "rust-src"]; };
+          rustToolchain = final.rust-bin.nightly.latest.default.override { extensions = [ "rust-src" ]; };
         })
       ];
 
       # Helpers for producing system-specific outputs
       supportedSystems = [ "x86_64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit overlays system; };
-      });
-    in {
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit overlays system; };
+          }
+        );
+    in
+    {
       # Schemas tell Nix about the structure of your flake's outputs
       schemas = flake-schemas.schemas;
 
       # Development environments
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          # Pinned packages available in the environment
-          packages = with pkgs; [
-            rustToolchain
-            cargo-bloat
-            cargo-edit
-            rust-analyzer
-          ];
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            # Pinned packages available in the environment
+            packages = with pkgs; [
+              rustToolchain
+              cargo-bloat
+              cargo-edit
+              rust-analyzer
+            ];
 
-          # Environment variables
-          env = {
-            RUST_BACKTRACE = "1";
-            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            # Environment variables
+            env = {
+              RUST_BACKTRACE = "1";
+              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            };
           };
-        };
-      });
+        }
+      );
     };
 }
