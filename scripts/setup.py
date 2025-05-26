58eb1a6308c0442d7d9132f589d5d3098124d7b6
lakrestofer <lakrestofer@gmail.com>
2025-05-26 17:12:32 +0200

    add setup.py

---
+++ scripts/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="flashcard_image",
+    version="1.0",
+    # Modules to import from other scripts:
+    packages=find_packages(),
+    # Executables
+    scripts=["flashcard_image.py"],
+)
